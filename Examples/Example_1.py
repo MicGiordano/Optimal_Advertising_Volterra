dda0ce17016b578dac0327de1125a1cf445dd265
MicGiordano <79482883+MicGiordano@users.noreply.github.com>
2021-03-14 10:40:58 +0100

    Update Example_1.py

--- Examples/Example_1.py
+++ Examples/Example_1.py
@@ -9,3 +9,12 @@ H=0.3
 
 
 V_a,V_0,t=Volterra(X_0,a,b,s,MRL,H,Compare=True)
+
+"""
+
+In this example we consider the standard Volterra-Ornstein–Uhlenbeck process, with parameters as above. 
+The main point is to show the difference between the advertised model V_a and the non-advertised model V_0.
+
+Here we consider the default advertising vector u, defined as u(t) = t for t in [2,6] and 0 elsewhere.
+
+"""
